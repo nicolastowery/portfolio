b1bafc3918ad464162e3bb573d0d56f24c0b9983
Nicolas Towery <nicolastowery@gmail.com>
2023-10-24 15:25:06 -0500

    Reverted router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
