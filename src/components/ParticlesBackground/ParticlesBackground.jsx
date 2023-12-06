615251581c7ee6d38a502bb48ada852666edc112
Nicolas Towery <nicolastowery@gmail.com>
2023-12-06 08:53:22 -0600

    Update ParticlesBackground.jsx

--- src/components/ParticlesBackground/ParticlesBackground.jsx
+++ src/components/ParticlesBackground/ParticlesBackground.jsx
@@ -39,10 +39,10 @@ const ParticlesBackground = () => {
               quantity: 4,
             },
             repulse: {
-              distance: 100, // 200 default
-              duration: 0.4, // 0.4 default
-              speed: 0.5,
-              maxSpeed: 0.8,
+              distance: 60, // 200 default
+              duration: 0.2, // 0.4 default
+              speed: 0.4,
+              maxSpeed: 0.4,
             },
             bounce: {
               distance: 200, // 200 default
@@ -56,7 +56,7 @@ const ParticlesBackground = () => {
           },
           links: {
             color: "#ffffff",
-            distance: 150,
+            distance: 200,
             enable: true,
             opacity: 0.6,
             width: 1,
@@ -66,25 +66,25 @@ const ParticlesBackground = () => {
             enable: true,
             outModes: {
               default: "bounce",
-              onHover: "stop",
+              onHover: "repulse",
             },
-            maxSpeed: 0.5,
+            maxSpeed: 0.35,
             random: false,
-            speed: 0.5, // 1.8
+            speed: 0.3, // 1.8
             straight: false,
           },
           number: {
             density: {
               enable: true,
-              area: 800, // 800
+              area: 1200, // 800
             },
-            value: 80, // 80
+            value: 40, // 80
           },
           opacity: {
             value: 0.1,
           },
           shape: {
-            type: "circle",
+            type: "",
           },
           size: {
             value: { min: 1, max: 3 },
